d62596fdb9ce7d227c1b54082ed2dd639eb4cbe2
kchu-projects <30435495+kchu-projects@users.noreply.github.com>
2020-10-20 21:33:50 +0900

    Create activity-10

---
+++ activity-10/fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci_iterative(num):
+    fib1 = fib2 = fib3 = 1
+
+    for _ in range(num - 2):
+        fib3 = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib3
+
+    return fib3
+
+
+print(fibonacci_iterative(2))
+print(fibonacci_iterative(3))
+print(fibonacci_iterative(10))
