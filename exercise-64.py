829d67ebd8535b308d5cc786fc61d9b2439a1c8c
kchu-projects <30435495+kchu-projects@users.noreply.github.com>
2020-10-23 21:46:02 +0900

    Create exercise-64.py

---
+++ exercise-64.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+grades = ['A', 'B', 'C', 'D', 'E', 'F']
+students_count = [20, 30, 10, 5, 8, 2]
+
+plt.title('Grades Bar Plot for Biology Class')
+plt.xlabel('Grade')
+plt.ylabel('Num Students')
+plt.bar(grades, students_count, color=['green', 'gray', 'gray', 'gray', 'gray', 'red'])
+plt.show()
