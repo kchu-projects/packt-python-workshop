a87bf0297f475e2379c3e0ab9ac41a607f19fb6e
kchu-projects <30435495+kchu-projects@users.noreply.github.com>
2020-11-01 19:26:58 +0900

    Create exercise-73.py

---
+++ exercise-73.py
@@ -0,0 +1,9 @@
+class Country:
+    def __init__(self, name="Unspecified", population=None, size_kmsq=None):
+        self.name = name
+        self.population = population
+        self.size_kmsq = size_kmsq
+
+usa = Country(name="United States of America", size_kmsq=9.8e6)
+
+print(usa.__dict__)
