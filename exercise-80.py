066d869be1aa3f5ac48d945c6aa0db0b14e75b20
kchu-projects <30435495+kchu-projects@users.noreply.github.com>
2020-11-02 14:43:19 +0900

    Create exercise-80.py

---
+++ exercise-80.py
@@ -0,0 +1,18 @@
+class Temperature:
+    def __init__(self, celsius):
+        self.celsius = celsius
+
+    @property
+    def fahrenheit(self):
+        return self.celsius * 9 / 5 + 32
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.celsius = (value - 32) * 5 / 9
+
+
+temp = Temperature(5)
+print(temp.fahrenheit)
+
+temp.fahrenheit = 32
+print(temp.celsius)
