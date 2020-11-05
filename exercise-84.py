cc42d4e8ef73dfc24514e656fa1bea009fa73550
kchu-projects <30435495+kchu-projects@users.noreply.github.com>
2020-11-05 13:56:17 +0900

    Create exercise-84.py

---
+++ exercise-84.py
@@ -0,0 +1,40 @@
+import datetime
+
+
+class Person:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+class Baby(Person):
+    def speak(self):
+        print("Blah blah blah")
+
+
+class Adult(Person):
+    def speak(self):
+        print(f"Hello, my name is {self.first_name}")
+
+
+class Calendar:
+    def book_appointment(self, date):
+        print(f"Booking appointment for date {date}")
+
+
+class OrganizedAdult(Adult, Calendar):
+    pass
+
+
+class OrganizedBaby(Baby, Calendar):
+    def book_appointment(self, date):
+        print("Note that you are booking an appointment with a baby.")
+        super().book_appointment(date)
+
+
+andres = OrganizedAdult("Andres", "Gomez")
+boris = OrganizedBaby("Boris", "Bumblebutton")
+
+andres.speak()
+boris.speak()
+boris.book_appointment(datetime.date(2018, 1, 1))
