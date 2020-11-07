94f17d3afe53eea3235e8c5604c8107f7fb2791c
kchu-projects <30435495+kchu-projects@users.noreply.github.com>
2020-11-07 21:36:23 +0900

    Create exercise-85.py

---
+++ exercise-85.py
@@ -0,0 +1,16 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class Point:
+    x: int
+    y: int
+
+
+p = Point(x=10, y=20)
+print(p)
+
+p2 = Point(x=10, y=20)
+print(p == p2)
+
+print(dataclasses.asdict(p))
