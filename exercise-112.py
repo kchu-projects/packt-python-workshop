14556c1669a0745449956bb8442e28dc8c94cc40
kchu-projects <30435495+kchu-projects@users.noreply.github.com>
2020-11-16 12:28:06 +0900

    Update exercise-112.py

--- exercise-112.py
+++ exercise-112.py
@@ -1,18 +1,24 @@
+from decimal import Decimal, getcontext
+getcontext().prec = 3
+
+
+base_rate = Decimal(0.10)
+
 def _manager_adjust(salary, rise):
-    if rise < 0.10:
+    if rise < base_rate:
         # We need to keep the managers happy
-        return 0.10
+        return base_rate
 
     if salary >= 1000000:
         # They are making enough already
-        return rise - 0.10
+        return rise - base_rate
 
 
 def calculate_new_salary(salary, promised_pct, is_manager, is_good_year):
     rise = promised_pct
     # remove 10% if it was a bad year
     if not is_good_year:
-        rise -= 0.1
+        rise -= base_rate
     else:
         pass
 
@@ -21,12 +27,12 @@ def calculate_new_salary(salary, promised_pct, is_manager, is_good_year):
         rise = _manager_adjust(salary, rise)
 
     # extra bonus for people with high rises
-    if rise >= 0.20:
-        rise = rise + 0.10
+    if rise >= base_rate * 2:
+        rise = rise + base_rate
 
     salary_increase = salary * rise
     return int(salary + salary_increase)
 
 
-rose_salary = calculate_new_salary(1_000_000, 0.30, True, True)
+rose_salary = calculate_new_salary(1_000_000, Decimal(0.30), True, True)
 print(f"Rose's salary will be: {rose_salary}")
