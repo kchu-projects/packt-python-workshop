3a1c87cbf23d74f84acff380454f6fa1092c0eb2
kchu-projects <30435495+kchu-projects@users.noreply.github.com>
2020-11-16 20:02:24 +0900

    Create exercise-114

---
+++ exercise-114/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="kchu_package",
+    version="1.0.0",
+    author="Kevin Clyde Chu",
+    author_email="kchu.projects@gmail.com",
+    description="Packt Python Workshop example package",
+    long_description="My package's longer description and will appear in the web.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
